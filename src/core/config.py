ebe9346da29fe813ad6de16a2f0c3454afc50f89
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-04 12:38:32 +0200

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,15 @@
-# En src/core/config.py
-from python-dotenv-vault import load_dotenv
-load_dotenv(override=True, vault_file=".env.vault")
+# Import the necessary classes
+from some_module import CANBusReader, K8sOrchestrator, GRPCHandler, ThreeDVisualizer
+
+class DataFlow4D:
+    def __init__(self):
+        self.layers = {
+            'physical': CANBusReader(),
+            'platform': K8sOrchestrator(),
+            'services': GRPCHandler(),
+            'app': ThreeDVisualizer()
+        }
+    
+    def process(self, vehicle_signal):
+        return [layer.transform(vehicle_signal) 
+                for layer in self.layers.values()]
