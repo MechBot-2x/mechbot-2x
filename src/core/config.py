d525b9cf66349ee7126a8707c2f3c8cbd0baa578
mechmind-dwv <ia.mechmind@gmail.com>
2025-04-04 12:40:32 +0200

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,15 +1,4 @@
-# Import the necessary classes
-from some_module import CANBusReader, K8sOrchestrator, GRPCHandler, ThreeDVisualizer
-
-class DataFlow4D:
-    def __init__(self):
-        self.layers = {
-            'physical': CANBusReader(),
-            'platform': K8sOrchestrator(),
-            'services': GRPCHandler(),
-            'app': ThreeDVisualizer()
-        }
-    
-    def process(self, vehicle_signal):
-        return [layer.transform(vehicle_signal) 
-                for layer in self.layers.values()]
+[lint]
+ignore = ["some_ignore_rule"]
+select = ["some_select_rule"]
+unfixable = ["some_unfixable_rule"]
